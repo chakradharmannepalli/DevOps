f33c7d1cec0de2c6d8d69ad242fce8d4dba98cbb
Ubuntu <ubuntu@ip-172-31-95-8.ec2.internal>
2025-04-27 13:44:49 +0000

    first

---
+++ task2/terraform/main.tf
@@ -0,0 +1,29 @@
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 5.0"
+
+  name = "${var.app_name}-vpc"
+  cidr = var.vpc_cidr
+
+  azs             = ["${var.aws_region}a", "${var.aws_region}b"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  tags = {
+    Name = "${var.app_name}-vpc"
+  }
+}
+
+module "ecs" {
+  source = "./modules/ecs"
+
+  app_name        = var.app_name
+  vpc_id          = module.vpc.vpc_id
+  private_subnets = module.vpc.private_subnets
+  public_subnets  = module.vpc.public_subnets
+  container_image = var.container_image
+  container_port  = var.container_port
+}
